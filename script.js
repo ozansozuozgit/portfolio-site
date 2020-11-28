21e36fbfc153bc3a9ffbd7e146c84e738129364b
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-29 00:09:04 +0300

    add navbar animation and toggle

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 gsap.registerPlugin(ScrollTrigger);
 let tl = gsap.timeline();
 
-tl.from('.small-circles', {
-  stagger: 0.1,
-  opacity: 0,
-  ease: Power4.easeOut,
-  duration: 1,
-});
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
 
-tl.to(
-  '.small-circles',
-  {
-    opacity: 0,
-    display: 'none',
-    duration: 0.2,
-  },
-  '+=0'
-);
+  // Toggle Nav
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    if (nav.classList.contains('nav-active')) {
+      tl.fromTo(
+        navLinks,
+        {
+          opacity: 0,
+          x: 100,
+        },
+        {
+          duration: 1,
+          delay: 1,
+          stagger: 0.2,
+          opacity: 1,
+          x: 0,
+          ease: 'elastic.out(1, 0.3)',
+        },
+        '-0.35'
+      );
+    } else {
+      tl.clear();
+    }
+  });
+};
+
+navSlide();
