0d39d5582402ee82191550c9b4e4a559462f1008
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-11-29 23:07:27 +0300

    change primary colors

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-gsap.registerPlugin(ScrollTrigger);
-let tl = gsap.timeline();
-
 const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
@@ -23,9 +20,12 @@ const navSlide = () => {
 };
 
 navSlide();
-
+const profilePic = document.querySelector('#landing img');
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml6 .letters');
+const name = document.querySelector('.letter:nth-child(4)');
+console.log(name);
+
 textWrapper.innerHTML = textWrapper.textContent.replace(
   /\S/g,
   "<span class='letter'>$&</span>"
@@ -37,4 +37,26 @@ anime.timeline({ loop: false }).add({
   translateZ: 0,
   duration: 750,
   delay: (el, i) => 50 * i,
+});
+anime.timeline({ loop: false }).add(
+  {
+    targets: [
+      '.letter:nth-child(4)',
+      '.letter:nth-child(5)',
+      '.letter:nth-child(6)',
+      '.letter:nth-child(7)',
+    ],
+
+    translateY: ['2.1em', 0],
+    duration: 1000,
+    color: '#F3AC59',
+  },
+  '+=1500'
+);
+anime.timeline({ loop: false }).add({
+  targets: profilePic,
+  opacity: [0, 1],
+  scale: [0.2, 1],
+  durationIn: 1750,
+  delay: 500,
 });
